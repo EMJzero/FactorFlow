1c1184fdceac70efaf963391a2bac9d5b7a527fe
EMJzero <marcoron00@gmail.com>
2024-09-27 11:21:54 +0200

    Added: explore random mappings, Energy method in Engine

--- comparisons/explore_random_mappings.py
+++ comparisons/explore_random_mappings.py
@@ -58,93 +58,44 @@ def parse_options():
         "help": args_match_and_remove("-h") or args_match_and_remove("--help"),
         "max_tries": args_match_and_remove("-mt", True) or args_match_and_remove("--max_tries", True),
         "random_moves": args_match_and_remove("-rm", True) or args_match_and_remove("--random_moves", True),
-        "store_init_conds": args_match_and_remove("-sic") or args_match_and_remove("--store_init_conds"),
-        "all_comps": args_match_and_remove("-ac") or args_match_and_remove("--all_comps")
+        "store_mappings": args_match_and_remove("-sm") or args_match_and_remove("--store_mappings"),
+        "all_comps": args_match_and_remove("-ac") or args_match_and_remove("--all_comps"),
+        "print_arrays": args_match_and_remove("-pa") or args_match_and_remove("--print_arrays")
     }
     return options
 
-"""
-def partitions(arr, n, seen=None):
-    if seen is None:
-        seen = set()
-    if n == len(arr):
-        yield [[x] for x in arr]
-    elif n == 1:
-        yield [arr]
-    else:
-        for i in range(1, len(arr)-n+2):
-            for first in combinations(range(len(arr)), i):
-                first_set = set(first)
-                arr1 = [arr[j] for j in first]
-                arr2 = [arr[j] for j in range(len(arr)) if j not in first_set]
-                for rest in partitions(arr2, n-1, seen):
-                    partition = [arr1] + rest
-                    partition_str = str(partition)
-                    if partition_str not in seen:
-                        seen.add(partition_str)
-                        yield partition
-
-def disjoint_partitions(arr1, arr2, arr3, n):
-    part1 = list(partitions(arr1, n))
-    print("Generation permutations 1/3...")
-    part2 = list(partitions(arr2, n))
-    print("Generation permutations 2/3...")
-    part3 = list(partitions(arr3, n))
-    print("Generation permutations 3/3...")
-    print("Generating combinations...")
-
-    all_partitions = list(product(part1, part2, part3))
-    random.shuffle(all_partitions)
-
-    for partition in all_partitions:
-        yield partition
-
-def randomFactorsInitializations(arch, comp):
-    initFactors(arch, comp)
-    enforceFactorsConstraints(arch)
-    setupBypasses(arch)
-    updateInstances(arch)
-    
-    mems = list(filter(lambda l : isinstance(l, MemLevel), arch))
-    random_disjoint_primes_lists = disjoint_partitions(primeFactorsList(arch[0].factors.dimProduct('D')), primeFactorsList(arch[0].factors.dimProduct('D')), primeFactorsList(arch[0].factors.dimProduct('D')), len(mems))
-    #print(primeFactorsList(arch[0].factors.dimProduct('D')), primeFactorsList(arch[0].factors.dimProduct('D')), primeFactorsList(arch[0].factors.dimProduct('D')), len(mems))
-    #print(list(random_disjoint_primes_lists))
-    
-    for disjoint_primes_list in random_disjoint_primes_lists:
-        dim_to_idx = {'D': 0, 'E': 1, 'L': 2}
-        for dim in dim_to_idx.keys():
-            for level_idx in range(len(mems)):
-                factors = Factors()
-                for prime in disjoint_primes_list[dim_to_idx[dim]][level_idx]:
-                    factors.addFactor(dim, prime, 1)
-                arch[level_idx].factors = factors
-        yield copy.deepcopy(arch)
-"""
+def randomDataflows(arch):
+    for level in arch:
+        if isinstance(level, MemLevel):
+            level.dataflow = random.sample(interleave(level.dataflow_constraints, [dim for dim in level.dataflow if dim not in level.dataflow_constraints]), 1)[0]
+        #else:
+        #    level.dataflow = random.shuffle(level.dataflow)
 
 # Random-ish but faster
-def randomFactorsInitializationsFast(arch, comp, random_moves = 10):
+def randomFactorsInitializationsFast(arch, comp):
     initFactors(arch, comp)
     enforceFactorsConstraints(arch)
     setupBypasses(arch)
     updateInstances(arch)
     
-    mems = list(filter(lambda l : isinstance(l, MemLevel), arch))
-    factors = reduce(lambda l, a : l + a, [[(dim, f) for f in mems[0].factors.toList(dim)] for dim in ['D', 'E', 'L']], [])
+    factors = reduce(lambda l, a : l + a, [[(dim, f) for f in arch[0].factors.toList(dim)] for dim in ['D', 'E', 'L']], [])
     
-    def randomMoves(arch, n):
+    def randomMoves(arch):
         random.shuffle(factors)
         for dim, factor in factors:
-            for _ in range(n):
-                dst_level_idx = random.choice(range(len(mems)))
+            choices = list(range(len(arch)))
+            random.shuffle(choices)
+            for dst_level_idx in choices:
                 if dst_level_idx == 0 or moveFactor(arch, 0, dst_level_idx, dim, factor, 1):
                     break
-    
-    already_seen = [hashFromFactors(arch)]
+
+    already_seen = []
     
     fails = 0
     while True:
         random_arch = copy.deepcopy(arch)
-        randomMoves(random_arch, random_moves)
+        randomMoves(random_arch)
+        randomDataflows(arch)
         hash = hashFromFactors(random_arch)
         if hash not in already_seen:
             already_seen.append(hash)
@@ -176,12 +127,13 @@ def randomFactorsInitializationsSlow(arch, comp, random_moves = 10):
                     if dst_level_idx == 0 or moveFactor(arch, 0, dst_level_idx, dim, factor, 1):
                         break
     
-    already_seen = [hashFromFactors(arch)]
+    already_seen = []
     
     fails = 0
     while True:
         random_arch = copy.deepcopy(arch)
         randomMoves(random_arch, random_moves)
+        randomDataflows(arch)
         hash = hashFromFactors(random_arch)
         if hash not in already_seen:
             already_seen.append(hash)
@@ -209,15 +161,16 @@ if __name__ == "__main__":
         print("Supported options:")
         print("-h, --help\t\t\tDisplay this help menu.")
         print("-mt, --max_tries <tries>\tSets to <tries> the number of starting point tried. Default is 10000.")
-        print("-rm, --random_moves <moves>\tSets to <moves> the number of moves attempted for each prime factor in the mapping. Default is 20.")
-        print("-sic, --store_init_conds\tIf given, the initial random starting points are also stored and displayed at the end.")
+        print("-rm, --random_moves <moves>\tSets to <moves> the number of moves attempted for each prime factor in the mapping. Default is 20. ->DEPRECATED<-")
+        print("-sm, --store_mappings\tIf given, the generated mappings are also stored and displayed at the end.")
         print("-ac, --all_comps\t\tTries all computations for the specified arch, and summarizes results in a table.")
+        print("-pa, --print_arrays\tPrints all the stored EDP and Wart values. Only works without '-ac'.")
         sys.exit(1)
 
     MAX_TRIES = int(options["max_tries"]) if options["max_tries"] else MAX_TRIES
     DUPLICATES_TO_STOP = MAX_TRIES*10
     RANDOM_MOVES = int(options["random_moves"]) if options["random_moves"] else RANDOM_MOVES
-    STORE_INITIAL_CONDITIONS = STORE_INITIAL_CONDITIONS or options["store_init_conds"]
+    STORE_INITIAL_CONDITIONS = STORE_INITIAL_CONDITIONS or options["store_mappings"]
 
     supported_archs = ["gemmini", "eyeriss", "simba", "tpu"]
     if len(sys.argv) < 2 or sys.argv[1] not in supported_archs:
@@ -239,50 +192,25 @@ if __name__ == "__main__":
 
     # << Gemmini >>
     if arch_name == "gemmini":
-        base_arch = arch_gemmini
-        arch = arch_gemmini_factorflow_2
+        arch = arch_gemmini
 
     # << Eyeriss >>
     elif arch_name == "eyeriss":
-        base_arch = arch_eyeriss
-        arch = arch_eyeriss_factorflow_1
-        arch[2].dims = ['D', 'E']
-        arch[2].dataflow = ['D', 'E']
+        arch = arch_eyeriss
     
     # << Simba >>
     elif arch_name == "simba":
-        base_arch = arch_simba
-        arch = arch_simba_factorflow_1
-        arch[2].dims = ['D', 'E']
-        arch[2].dataflow_constraints = ['D', 'E']
+        arch = arch_simba
     
     # << TPU >>
     elif arch_name == "tpu":
-        base_arch = arch_tpu
-        arch = arch_tpu_factorflow_1
-        Settings.STEPS_TO_EXPLORE = 2
-        print(f"INFO: tpu selected, forcefully updating setting STEPS_TO_EXPLORE to {Settings.STEPS_TO_EXPLORE}.")
-        #arch[0].dataflow_constraints = ['L', 'D', 'E']
-        #arch[1].dataflow_constraints = ['D', 'E', 'L']
-        #arch[2].dataflow_constraints = ['L', 'E', 'D']
-        #arch[3].dataflow_constraints = ['L', 'E', 'D']
-        #arch[5].dataflow_constraints = ['L', 'D', 'E']
-        #arch[7].dataflow_constraints = ['D', 'E', 'L']
-        #arch[0].dataflow = ['L', 'D', 'E']
-        #arch[1].dataflow = ['D', 'E', 'L']
-        #arch[2].dataflow = ['L', 'E', 'D']
-        #arch[3].dataflow = ['L', 'E', 'D']
-        #arch[5].dataflow = ['L', 'D', 'E']
-        #arch[7].dataflow = ['D', 'E', 'L']
+        arch = arch_tpu
     
-    for level_idx in range(len(arch)):
-        #if not isinstance(arch[level_idx], ComputeLevel):
-        arch[level_idx].factors_contraints = base_arch[level_idx].factors_contraints
     #Here changing settings is fine, there are no processes
     Settings.forcedSettingsUpdate(arch, False)
     
     if options["all_comps"]:
-        table = PrettyTable(["Comp", "Arch", "All on Level 0 - EDP", "Random - Min EDP", "Random - Max EDP", "Random - Avg. EDP"])
+        table = PrettyTable(["Comp", "Arch", "FF - EDP", "Random - Min EDP", "Random - Max EDP", "Random - Avg. EDP"])
     for comp_name, comp in comps.items():
         print(f"Working on comp: {comp_name} and arch: {arch_name}...")
         tried = 0
@@ -294,15 +222,15 @@ if __name__ == "__main__":
         arch_copy = copy.deepcopy(arch)
         if fitConstraintsToComp(arch_copy, comp, arch_name, comp_name):
             continue
-        random_archs = randomFactorsInitializationsFast(arch_copy, comp, RANDOM_MOVES)
+        random_archs = randomFactorsInitializationsFast(arch_copy, comp)
         #_ = next(random_archs)
-        print(f"Starting optimization of {MAX_TRIES} different starting points:")
+        print(f"Starting optimization of {MAX_TRIES} random mappings:")
         for current_arch in random_archs:
             try:
                 assert not findConstraintsViolation(current_arch, False)
                 if STORE_INITIAL_CONDITIONS: initial_conditions.append(factorsString(current_arch))
-                current_arch, wart = factorFlow(current_arch, comp, bias_read, already_initialized = True)
                 edp = EDP(current_arch, bias_read, True)
+                wart = Wart(current_arch, comp, bias_read)
             except AssertionError:
                 continue
             warts.append(wart)
@@ -316,36 +244,38 @@ if __name__ == "__main__":
                 tried += 1
         
         if not options["all_comps"]:
-            print(f"FF INIT: {factorsString(arch)}")
-            arch, factorflow_wart = factorFlow(arch, comp, bias_read)
-            print(f"FF FINAL: {factorsString(arch)}")
-            factorflow_edp = EDP(arch, bias_read, True)
+            print(f"FF INIT: ", end="")
+            printFactors(arch)
+            factorflow_edp, _, _, _, _, _, arch = run_engine(arch, comp, bias_read, False)
+            factorflow_wart = Wart(arch, comp, bias_read)
+            print(f"FF FINAL: ", end="")
+            printFactors(arch)
             
             print("\nResults for Wart (higher is better):")
-            print(f"All factors on first level - Wart: {factorflow_wart:.3e}")
-            print(f"Random starting points:\n\t- avg. Wart: {sum(warts)/len(warts):.3e}\n\t- min Wart: {min(warts):.3e}\n\t- max Wart: {max(warts):.3e}")
+            print(f"FF result - Wart: {factorflow_wart:.3e}")
+            print(f"Random mappings:\n\t- avg. Wart: {sum(warts)/len(warts):.3e}\n\t- min Wart: {min(warts):.3e}\n\t- max Wart: {max(warts):.3e}")
             short_warts = [f"{w:.3e}" for w in warts]
             if STORE_INITIAL_CONDITIONS:
                 for initial_condition, wart in zip(initial_conditions, short_warts):
-                    print(f"Initial condition: {initial_condition}, Wart: {wart}")
-            #else:
-            #    print(f"\nComplete random starting point Warts: {short_warts}")
+                    print(f"Mapping: {initial_condition}, Wart: {wart}")
+            if options["print_arrays"]:
+                print(f"\nComplete mappings Warts: {short_warts}")
             
             print("\nResults for EDP (lower is better):")
-            print(f"All factors on first level - EDP: {factorflow_edp:.3e}")
-            print(f"Random starting points:\n\t- avg. EDP: {sum(edps)/len(edps):.3e}\n\t- min EDP: {min(edps):.3e}\n\t- max EDP: {max(edps):.3e}")
+            print(f"FF result - EDP: {factorflow_edp:.3e}")
+            print(f"Random mappings:\n\t- avg. EDP: {sum(edps)/len(edps):.3e}\n\t- min EDP: {min(edps):.3e}\n\t- max EDP: {max(edps):.3e}")
             short_edps = [f"{e:.3e}" for e in edps]
             if STORE_INITIAL_CONDITIONS:
                 for initial_condition, edp in zip(initial_conditions, short_edps):
-                    print(f"Initial condition: {initial_condition}, EDP: {edp}")
-            #else:
-            #    print(f"\nComplete random starting point Warts: {short_warts}")
+                    print(f"Mapping: {initial_condition}, EDP: {edp}")
+            if options["print_arrays"]:
+                print(f"\nComplete mappings EDPs: {short_edps}")
         else:
             arch_ff = copy.deepcopy(arch)
             if fitConstraintsToComp(arch_ff, comp, arch_name, comp_name):
                 continue
-            arch_ff, factorflow_wart = factorFlow(arch_ff, comp, bias_read)
-            factorflow_edp = EDP(arch_ff, bias_read, True)
+            factorflow_edp, _, _, _, _, _, arch_ff = run_engine(arch_ff, comp, bias_read, False)
+            factorflow_wart = Wart(arch_ff, comp, bias_read)
             table.add_row([comp_name, arch_name.title(), f"{factorflow_edp:.3e}", f"{min(edps):.3e}", f"{max(edps):.3e}", f"{sum(edps)/len(edps):.3e}"])
     if options["all_comps"]:
         print(table)
